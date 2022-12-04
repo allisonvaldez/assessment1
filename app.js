236f4ac8e5925f4792be038cf50ab43a76149ca1
Allison Valdez <ajsnipes@usfca.edu>
2022-12-03 19:46:36 -0500

    resume working

--- app.js
+++ app.js
@@ -1,37 +1,39 @@
 /*
+QUESTIONS:
+1. when should i use event delegation?)
+2. diff btw event delegation and eventListener
+*/
+
+
+/*
  Create the elements of the DOM:
  1. formElement creates the form element
  2. textTop gathers text input from the form element
- 2. urlInput gathers url input from the form element
- 2. textBottom gathers text input from the form element
- 3. placeMeme places the meme below the form
- 4. removeMeme removes the meme and text (but need to create element fitst)
+ 3. urlInput gathers url input from the form element
+ 4. textBottom gathers text input from the form element
+ 5. placeMeme places the meme below the form
+ 6. removeMeme removes the meme and text (but need to create element fitst)
  */
 
 const formElement = document.querySelector("#form");
 const textTop = document.querySelector('input[name="tTop"]');
 const urlInput = document.querySelector('input[name="url-link"]');
-const urlInput = document.querySelector('input[name="tBottom"]');
+const textBottom = document.querySelector('input[name="tBottom"]');
 const placeMeme = document.querySelector("#memePlacement");
 const removeMeme = document.createElement("button");
 
-/*
-each action should have its event delegation (when should i use event delegation?) 
-*/
-
 /* 
 each action should have its own function:
-
-function to gather top text for meme
-function to gather img text for meme
-function to gather bottom text for meme
+1. function to gather top text for meme
+2. function to gather img text for meme
+3. function to gather bottom text for meme
 
 */
 
-//function to gather top text for meme
+//gather top text for meme
 function gatherTopText(textOne) {
     //create an element to store gathered text from input for top
-    const textTopOne = document.createElement('h6');
+    const textTopOne = document.createElement('h2');
 
     // set inner text of created h6 element from input
     textTopOne.innerText = textOne;
@@ -40,12 +42,20 @@ function gatherTopText(textOne) {
     return textTopOne;
 }
 
-//function to gather img text for meme
+/* gather img text for meme source: https://stackoverflow.com/questions/17634019/javascript-load-an-image-from-url-and-display */
+function gatherImg() {
+    const val = document.getElementById("url-link");
+    const src = urlInput + val;
+    const img = document.createElement('img');
 
-//function to gather bottom text for meme
-function gatherTopText(textTwo) {
+    img.src = src;
+    document.body.appendChild(img);
+}
+
+// gather bottom text for meme
+function gatherBottomText(textTwo) {
     //create an element to store gathered text from input for bottom
-    const textBottomOne = document.createElement('h6');
+    const textBottomOne = document.createElement('h2');
 
     // set inner text of created h6 element from input
     textBottomOne.innerText = textTwo;
@@ -54,7 +64,45 @@ function gatherTopText(textTwo) {
     return textBottomOne;
 }
 
-//submit button on click
 
-// add the three together prepend append???
+/*
+each action should have its event delegation:
+1. remove button
+2. click sumbit add the three together prepend append??? on sumbit?
 
+*/
+
+//remove button
+
+// click event for sumbit button
+formElement.addEventListener("submit", function (e) {
+    e.preventDefault();
+    console.log("submit triggered");
+
+    /* 
+    1. firstText saves the input for the 1st textbox from into a variable
+    2. placeMeme appends it to dedicated place in DOM
+    3. we clear first text form
+    */
+    const firstText = gatherTopText(textTop.value);
+    placeMeme.appendChild(firstText);
+    textTop.value = "";
+
+    /* 
+    1. populateImg saves the url input for img into a variable
+    2. placeMeme appends it to dedicated place in DOM
+    3. we clear first text form
+    */
+    const populateImg = gatherImg(urlInput.value);
+    placeMeme.appendChild(populateImg);
+    urlInput.value = "";
+
+    /* 
+    1. secondText saves the input for the 1st textbox from into a variable
+    2. placeMeme appends it to dedicated place in DOM
+    3. we clear third text form
+    */
+    const secondText = gatherBottomText(textBottom.value);
+    placeMeme.appendChild(secondText);
+    textBottom.value = "";
+})
